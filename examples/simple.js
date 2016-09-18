c15176a0d884bf332603b715cb38775a08e26e43
Forrest Desjardins <desjardinsfg@gmail.com>
2016-09-17 21:30:58 -0800

    Improve simple example

--- examples/simple.js
+++ examples/simple.js
@@ -1,42 +1,39 @@
 const express = require('express');
 const request = require('request');
-const proxycache = require('../')
+const proxycache = require('../');
 
-// create gcloud proxy cache client
+// Create Redis/Cloud Storage proxycache client
 const config = {
-  store: {
-    client: 'redis',
-    connection: {}
-  },
-  cache: {
-    client: 'gcloud',
-    connection: {
-      keyFilename: './gcloud-key.json',
-      projectId: 'my-project-id'
-    },
-    options: {
+	store: {
+		client: 'redis',
+		connection: {}
+	},
+	cache: {
+		client: 'gcloud',
+		connection: {
+			keyFilename: './gcloud-key.json',
+			projectId: 'my-project-id'
+		},
+		options: {
 			bucket: 'images'
 		}
-  }
-}
-let cache
-proxycache(config).then(pc => cache = pc)
+	}
+};
+let cache;
+proxycache(config).then(client => {
+	cache = client;
+});
 
-// downstream service
-const getDownstreamUrl = (id) => {
-  return Promise.resolve(`http://localhost:3888/images/${id}`)
-}
-
-// image server
-const imgsrv = express()
+// Image server to proxy for
+const imgsrv = express();
 imgsrv.get('/images/:id', (req, res) => {
-  const id = req.params.id
-  request.get(`http://www.fillmurray.com/g/${id}/${id}`).pipe(res)
-})
-imgsrv.listen(3888)
-console.log('imgsrv listening on 3888')
+	const id = req.params.id;
+	request.get(`http://www.fillmurray.com/g/${id}/${id}`).pipe(res);
+});
+imgsrv.listen(3888);
+console.log('Image server listening on 3888');
 
-const template = (src) => `
+const render = src => `
 <html>
 	<head>
 		<title>bfm</title>
@@ -45,25 +42,24 @@ const template = (src) => `
 		<img src='${src}' height=500 width=500/>
 	</body>
 </html>
-`
+`;
 
-// api server
-const app = express()
+// API Server
+const app = express();
 app.get('/images/:id', (req, res) => {
-  const id = req.params.id
-  cache.get(id).then(result => {
-    // cache hit
-    if (result) {
-      return res.end(template(result))
-    }
-    // cache miss
-    getDownstreamUrl(id).then(url => {
-      // send the default url
-      res.end(template(url))
-      // tell proxy-cache to cache the file
-      cache.set(id, url, 60)
-    })
-  })
-})
-app.listen(8000)
-console.log('app listening on 8000')
+	const id = req.params.id;
+	// query the cache
+	cache.get(id).then(result => {
+		// cache hit
+		if (result) {
+			return res.end(render(result));
+		}
+		const uri = `http://localhost:3888/images/${id}`;
+			// cache miss; send the default url
+		res.end(render(uri));
+		// cache the file
+		cache.set(id, uri, 60);
+	});
+});
+app.listen(8000);
+console.log('API server listening on 8000');
